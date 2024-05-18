f7a69da20d4a457fc891a0b1da7f6e831e665310
Prikshit singh <prikshitsingh79@gmail.com>
2024-05-19 00:12:42 +0530

    Fix frontend as submodule bug

---
+++ packages/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <div className="Title">YourStories</div>
+    <hr/>
+    </div>
+  );
+}
+
+export default App;
