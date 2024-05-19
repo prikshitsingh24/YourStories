0801cc64779c2beb42a36e88b9ac4212bc903c42
Prikshit singh <prikshitsingh79@gmail.com>
2024-05-19 12:38:31 +0530

    Add typescript to Backend and hotreload

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
