6fa3db1d40719618a0013342250bea9e758e8098
wolverinex24 <singhpriyanshu783@gmail.com>
2024-05-19 20:04:47 +0530

    Chane port backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import login from './routes/user/login';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 
 app.use('/user', login);
